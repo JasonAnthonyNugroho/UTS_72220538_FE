35169c0c735c0686788af9a6d163ea4400c9f839
itsHikariii <hasonanthonynugroho88@gmail.com>
2024-10-27 23:13:42 +0700

    Add project files.

---
+++ UTS_72220538/Course.cs
@@ -0,0 +1,15 @@
+﻿using UTS_72220538.Components.Pages;
+
+namespace UTS_72220538
+{
+    public class Course
+    { 
+        public int couseId { get; set; }
+        public string name { get; set; }
+        public string imageName { get; set; }
+        public int duration { get; set; }
+        public string description { get; set; }
+        public Categories categories { get; set; }
+
+    }
+}
